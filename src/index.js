b9b099fc4e667753d279a8581d8eb5c287a682eb
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-10-21 16:35:37 +0500

    zamaan-app

--- src/index.js
+++ src/index.js
@@ -3,13 +3,10 @@ import ReactDOM from 'react-dom/client';
 import "bootstrap/dist/css/bootstrap.min.css";
 import './sass/main.scss';
 import App from './App';
-import { getLanguage } from './locales';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div className={`${getLanguage() === 'uz' ? 'uzActive' : ''}`}>
-      <App />
-    </div>
+    <App />
   </React.StrictMode>
 );
